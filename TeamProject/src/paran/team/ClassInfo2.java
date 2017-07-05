2cccdbcad14b75b3781bd85ac343ab26132a24aa
2강009 <2009@192.168.0.18>
2017-07-05 09:40:06 +0900

    ClssInfo 17/07/05 수정전

--- TeamProject/src/paran/team/ClassInfo2.java
+++ TeamProject/src/paran/team/ClassInfo2.java
@@ -16,20 +16,20 @@ public class ClassInfo2{
 	 
  	public static void main(String[] args){ 
  		
- 		System.out.println("1ë°?"); 
+ 		System.out.println("1ë°˜"); 
  		StuList class1 = new StuList("C:\\student.txt"); 
  		class1.showList(); 
  		 
- 		System.out.println("2ë°?"); 
+ 		System.out.println("2ë°˜"); 
  		StuList class2 = new StuList("C:\\student2.txt"); 
  		class2.showList(); 
  		
- 		System.out.println("?­?‚¹? •? ¬"); 
+ 		System.out.println("ë­í‚¹ì •ë ¬"); 
  		class1.sortList(); 
  		class2.sortList(); 
- 		System.out.println("1ë°?"); 
+ 		System.out.println("1ë°˜"); 
  		class1.showList(); 
- 		System.out.println("2ë°?"); 
+ 		System.out.println("2ë°˜"); 
  		class2.showList(); 
  		
  	} 
@@ -77,31 +77,31 @@ abstract class Student{
  		this.rank = rank; 
  	} 
  	public abstract void showInfo(); 
- 	//abstract = ?¸?Š¤?„´?Š¤ ?ƒ?„± ë¶ˆê? 
+ 	//abstract = ì¸ìŠ¤í„´ìŠ¤ ìƒì„± ë¶ˆê°€ 
 } 
  
  
-class DomeStudent extends Student{//êµ??‚´?•™?ƒ 
- 	private String resiId;//ì£¼ë?¼ë²ˆ?˜¸ 
+class DomeStudent extends Student{//êµ­ë‚´í•™ìƒ 
+ 	private String resiId;//ì£¼ë¯¼ë²ˆí˜¸ 
  
  	public DomeStudent(String name, String stuId, String resiId, int score){ 
  		super(name, stuId, score);  		 		
  		this.resiId=resiId; 
  	} 
  	public void showInfo(){ 
- 		System.out.println("?‚´êµ??¸ name => "+ getName() + " stuId => "+getStuId()+" resiId => "+resiId+" score = "+super.getScore()+" ranking = "+ super.getRank()); 
+ 		System.out.println("ë‚´êµ­ì¸ name => "+ getName() + " stuId => "+getStuId()+" resiId => "+resiId+" score = "+super.getScore()+" ranking = "+ super.getRank()); 
  	} 
 } 
  
  
-class ForeStudent extends Student{//êµ??™¸?•™?ƒ 
- 	private String foreignId;//ì£¼ë?¼ë²ˆ?˜¸ 
+class ForeStudent extends Student{//êµ­ì™¸í•™ìƒ 
+ 	private String foreignId;//ì£¼ë¯¼ë²ˆí˜¸ 
  	public ForeStudent(String name, String stuId, String foreignId, int score){ 
  		super(name, stuId, score); 
  		this.foreignId=foreignId; 
  	} 
  	public void showInfo(){ 
- 		System.out.println("?™¸êµ??¸ name => "+ getName() + " stuId => "+getStuId()+" resiId => "+foreignId+" score = "+super.getScore()+" ranking = "+ super.getRank()); 
+ 		System.out.println("ì™¸êµ­ì¸ name => "+ getName() + " stuId => "+getStuId()+" resiId => "+foreignId+" score = "+super.getScore()+" ranking = "+ super.getRank()); 
  	} 
 } 
 
@@ -117,43 +117,43 @@ class ForeStudent extends Student{//êµ??™¸?•™?ƒ
  	} 
  	
  	public void initStudent(String dir){ 
- 		// ?ŒŒ?¼ ë³??ˆ˜ file?„ ë§Œë“ ?‹¤ 
+ 		// íŒŒì¼ ë³€ìˆ˜ fileì„ ë§Œë“ ë‹¤ 
  		File file = new File(dir); 
      	try { 
-     		// BufferedReader ë³??ˆ˜?— file?„ ?„£?Š”?‹¤ 
+     		// BufferedReader ë³€ìˆ˜ì— fileì„ ë„£ëŠ”ë‹¤ 
      		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"euc-kr")); 
   
  
-         	// ?ŒŒ?¼?„ ?•œì¤„ì”© ?½?–´ ?„£ê¸? ?œ„?•œ ë³??ˆ˜ line 
+         	// íŒŒì¼ì„ í•œì¤„ì”© ì½ì–´ ë„£ê¸° ìœ„í•œ ë³€ìˆ˜ line 
          	String line = null; 
  
  
-         	// ?•˜?‚˜?˜ line?„ split ?•˜?—¬ ?„£?„ ë°°ì—´ splitedStr 
+         	// í•˜ë‚˜ì˜ lineì„ split í•˜ì—¬ ë„£ì„ ë°°ì—´ splitedStr 
          	String[] splitedStr = null; 
          	int score=0; 
  
  
-         	// ?•œ ì¤„ì”© ?½?–´?„œ line?— ?„£?? ?›„ null?´ ?•„?‹ˆë©? ?‹¤?–‰ 
+         	// í•œ ì¤„ì”© ì½ì–´ì„œ lineì— ë„£ì€ í›„ nullì´ ì•„ë‹ˆë©´ ì‹¤í–‰ 
          	while( (line = reader.readLine()) != null ) { 
  
  
-             	// ì´ˆê¸°?™” 
+             	// ì´ˆê¸°í™” 
              	splitedStr = null; 
  
  
-             	// ?ƒ­?„ ê¸°ì??œ¼ë¡? ?˜?¼?„œ splitedStr ?— ?„£?Š”?‹¤ 
+             	// íƒ­ì„ ê¸°ì¤€ìœ¼ë¡œ ì˜ë¼ì„œ splitedStr ì— ë„£ëŠ”ë‹¤ 
              	splitedStr = line.split("\t"); 
              	
  
-             	// ë°°ì—´?— ?“¤?–´ê°? ê¸¸ì´ ë§Œí¼ ë°˜ë³µ?•œ?‹¤ 
+             	// ë°°ì—´ì— ë“¤ì–´ê°„ ê¸¸ì´ ë§Œí¼ ë°˜ë³µí•œë‹¤ 
              	for (int i = 0; i < splitedStr.length; i++) { 
              		if(i==3){ 
-             			score = Integer.valueOf(splitedStr[3]);//? ?ˆ˜?Š” intë¡? 
+             			score = Integer.valueOf(splitedStr[3]);//ì ìˆ˜ëŠ” intë¡œ 
              		}else{ 
-             			// ?–‘ìª½ì˜ ê³µë°±?„ ? œê±°í•˜ê³? ?‹¤?‹œ ?…? ¥?•œ?‹¤ 
+             			// ì–‘ìª½ì˜ ê³µë°±ì„ ì œê±°í•˜ê³  ë‹¤ì‹œ ì…ë ¥í•œë‹¤ 
              			splitedStr[i] = splitedStr[i].trim(); 
              		} 
-                     // splitedStr ?„ List<Classëª?>?— ?…? ¥?•˜?Š” ?“± ?´?š©ê°??Š¥?•˜?‹¤ 
+                     // splitedStr ì„ List<Classëª…>ì— ì…ë ¥í•˜ëŠ” ë“± ì´ìš©ê°€ëŠ¥í•˜ë‹¤ 
              	} 
              	if(splitedStr[2].substring(0,1).equals("1")){ 
              		stuList.add(new DomeStudent(splitedStr[0],splitedStr[1],splitedStr[2],score)); 
@@ -172,8 +172,8 @@ class ForeStudent extends Student{//êµ??™¸?•™?ƒ
  	public void setRanking(){ 
  		
  		for(int i=0; i<stuList.size(); i++){             
-             for (int j = 0; j < stuList.size(); j++) { //ê¸°ì??°?´?„°?? ?‚˜ë¨¸ì??°?´?„°ë¹„êµ                              
-                 if(  stuList.get(i).getScore()<stuList.get(j).getScore()        ){   //ê¸°ì??°?´?„°ê°? ?‚˜ë¨¸ì??°?´?„°?¼ ë¹„êµ?–ˆ?„?•Œ ? ?œ¼ë©? ì¹´ìš´?Š¸ 
+             for (int j = 0; j < stuList.size(); j++) { //ê¸°ì¤€ë°ì´í„°ì™€ ë‚˜ë¨¸ì§€ë°ì´í„°ë¹„êµ                              
+                 if(  stuList.get(i).getScore()<stuList.get(j).getScore()        ){   //ê¸°ì¤€ë°ì´í„°ê°€ ë‚˜ë¨¸ì§€ë°ì´í„°ë¼ ë¹„êµí–ˆì„ë•Œ ì ìœ¼ë©´ ì¹´ìš´íŠ¸ 
                      stuList.get(i).setRank(stuList.get(i).getRank()+1); //COUNT                  
                  }               
              }           
